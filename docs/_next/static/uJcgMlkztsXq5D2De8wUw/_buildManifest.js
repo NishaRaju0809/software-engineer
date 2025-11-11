d243fc76b36aad5d6da3bbff4f3baae6c2837df6
Nisha Raju <nisharaju961@gmail.com>
2025-11-11 22:19:14 +0530

    Deploy portfolio site

---
+++ docs/_next/static/uJcgMlkztsXq5D2De8wUw/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/software-engineer//_next/:path+",
+        "destination": "/software-engineer/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
